1e3b3d5c157d4e893c6a9844c76eb3c7bc663a02
Snofts <samuelsnofts@gmail.com>
2025-10-21 05:28:44 +0100

    LatestComponent import adjusted in Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import Latestcollection from '../components/Latestcollection'
+import Latestcollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
